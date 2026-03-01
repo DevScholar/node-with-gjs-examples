47a820f7c7ac3930673def345a0ac0bd0aa2b048
DevScholar <devscholar@foxmail.com>
2026-03-01 17:37:52 +0800

    chore: remove build artifacts from git

--- dist/console/await-delay/await-delay.js
+++
@@ -1,8 +0,0 @@
-print('0s');
-await new Promise(resolve => setTimeout(resolve, 1000));
-print("1s");
-await new Promise(resolve => setTimeout(resolve, 1000));
-print("2s");
-await new Promise(resolve => setTimeout(resolve, 1000));
-print("3s");
-export {};
