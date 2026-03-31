3ab22bd33248ed12de934b7e2dabe0979bd747d5
DevScholar <devscholar@foxmail.com>
2026-03-31 12:45:59 +0800

    feat(ui): add menu and action features to the counter app
    Add the following features to the GTK and Adwaita versions of the counter app:
    - Reset counter action
    - About window
    - Menu bar (GTK) and pop-up menu (Adwaita)
    - Global label variable for updating actions

--- src/adwaita/counter/counter.ts
+++ src/adwaita/counter/counter.ts
@@ -1,7 +1,9 @@
 import Gtk from 'gi://Gtk?version=4.0';
 import Adw from 'gi://Adw?version=1';
+import Gio from 'gi://Gio';
 
 let clickCount = 0;
+let label: Gtk.Label;
 
 console.log("--- Adwaita Counter ---");
 
@@ -15,11 +17,60 @@ app.connect('activate', () => {
         default_height: 300
     });
 
+    // Actions
+    const resetAction = new Gio.SimpleAction({ name: 'reset' });
+    resetAction.connect('activate', () => {
+        clickCount = 0;
+        label.set_label('Clicks: 0');
+        console.log("Counter reset");
+    });
+    window.add_action(resetAction);
+
+    const aboutAction = new Gio.SimpleAction({ name: 'about' });
+    aboutAction.connect('activate', () => {
+        const aboutWindow = new Gtk.Window({
+            title: 'About',
+            transient_for: window,
+            modal: true,
+            default_width: 320,
+            default_height: 120
+        });
+        const box = new Gtk.Box({
+            orientation: Gtk.Orientation.VERTICAL,
+            spacing: 12,
+            halign: Gtk.Align.CENTER,
+            valign: Gtk.Align.CENTER
+        });
+        box.set_margin_start(24);
+        box.set_margin_end(24);
+        box.set_margin_top(24);
+        box.set_margin_bottom(24);
+        box.append(new Gtk.Label({ label: 'Node with GJS Counter Example' }));
+        const closeBtn = new Gtk.Button({ label: 'Close' });
+        closeBtn.connect('clicked', () => aboutWindow.close());
+        box.append(closeBtn);
+        aboutWindow.set_child(box);
+        aboutWindow.present();
+    });
+    window.add_action(aboutAction);
+
+    // Popover menu for the menu button
+    const menu = new Gio.Menu();
+    menu.append('Reset', 'win.reset');
+    menu.append('About', 'win.about');
+
+    const menuButton = new Gtk.MenuButton({
+        icon_name: 'open-menu-symbolic',
+        menu_model: menu
+    });
+
+    // Layout
     const toolbarView = new Adw.ToolbarView();
 
     const headerBar = new Adw.HeaderBar({
         title_widget: new Gtk.Label({ label: 'Adwaita Counter App' })
     });
+    headerBar.pack_end(menuButton);
     toolbarView.add_top_bar(headerBar);
 
     const box = new Gtk.Box({
@@ -33,7 +84,7 @@ app.connect('activate', () => {
     box.set_margin_top(20);
     box.set_margin_bottom(20);
 
-    const label = new Gtk.Label({
+    label = new Gtk.Label({
         label: 'Clicks: 0',
         css_classes: ['title-1']
     });
--- src/gtk/counter/counter.ts
+++ src/gtk/counter/counter.ts
@@ -1,6 +1,8 @@
 import Gtk from 'gi://Gtk?version=4.0';
+import Gio from 'gi://Gio';
 
 let clickCount = 0;
+let label: Gtk.Label;
 
 console.log("--- GTK4 Counter ---");
 
@@ -11,9 +13,61 @@ app.connect('activate', () => {
         application: app,
         title: 'GTK Counter App',
         default_width: 400,
-        default_height: 300
+        default_height: 300,
+        show_menubar: true
     });
 
+    // Actions
+    const resetAction = new Gio.SimpleAction({ name: 'reset' });
+    resetAction.connect('activate', () => {
+        clickCount = 0;
+        label.set_label('Clicks: 0');
+        console.log("Counter reset");
+    });
+    window.add_action(resetAction);
+
+    const aboutAction = new Gio.SimpleAction({ name: 'about' });
+    aboutAction.connect('activate', () => {
+        const aboutWindow = new Gtk.Window({
+            title: 'About',
+            transient_for: window,
+            modal: true,
+            default_width: 320,
+            default_height: 120
+        });
+        const box = new Gtk.Box({
+            orientation: Gtk.Orientation.VERTICAL,
+            spacing: 12,
+            halign: Gtk.Align.CENTER,
+            valign: Gtk.Align.CENTER
+        });
+        box.set_margin_start(24);
+        box.set_margin_end(24);
+        box.set_margin_top(24);
+        box.set_margin_bottom(24);
+        box.append(new Gtk.Label({ label: 'Node with GJS Counter Example' }));
+        const closeBtn = new Gtk.Button({ label: 'Close' });
+        closeBtn.connect('clicked', () => aboutWindow.close());
+        box.append(closeBtn);
+        aboutWindow.set_child(box);
+        aboutWindow.present();
+    });
+    window.add_action(aboutAction);
+
+    // Menu bar
+    const menubar = new Gio.Menu();
+
+    const counterMenu = new Gio.Menu();
+    counterMenu.append('Reset', 'win.reset');
+    menubar.append_submenu('Counter', counterMenu);
+
+    const helpMenu = new Gio.Menu();
+    helpMenu.append('About', 'win.about');
+    menubar.append_submenu('Help', helpMenu);
+
+    app.set_menubar(menubar);
+
+    // UI
     const box = new Gtk.Box({
         orientation: Gtk.Orientation.VERTICAL,
         spacing: 10,
@@ -25,7 +79,7 @@ app.connect('activate', () => {
     box.set_margin_top(20);
     box.set_margin_bottom(20);
 
-    const label = new Gtk.Label({
+    label = new Gtk.Label({
         label: 'Clicks: 0',
         css_classes: ['title-1']
     });
